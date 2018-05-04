c5091ce5660c849e25b6f70151407c8053094bed
Pinae <jonny@pinae.net>
2018-05-04 16:52:27 +0200

    Untested changes for better logging

--- train.py
+++ train.py
@@ -2,11 +2,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals, print_function
 from sacred import Experiment
+from sacred.observers import MongoObserver
+from sacred.utils import apply_backspaces_and_linefeeds
 from keras.optimizers import Adam
+from keras.losses import categorical_crossentropy
 from model import create_model
 from TrainingDataGenerator import UnsharpTrainingDataGenerator
+from secret_settings import mongo_url, db_name
 
 ex = Experiment("UnsharpDetector")
+ex.observers.append(MongoObserver.create(url=mongo_url, db_name=db_name))
+ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+
+@ex.capture
+def log_training_performance(_run, loss, lr):
+    _run.log_scalar("loss", float(loss))
+    _run.log_scalar("lr", float(lr))
+
+
+@ex.capture
+def log_validation_performance(_run, val_loss):
+    _run.log_scalar("validation_loss", float(val_loss))
+    _run.result = float(val_loss)
 
 
 @ex.config
@@ -21,15 +39,26 @@ def config():
     l2fs = (3, 3)
     l3fc = 32
     l3fs = (3, 3)
+    image_folders = [
+        "../../Bilder/gesammelte Landschaftsbilder/",
+        "../../Bilder/kleine Landschaftsbilder/",
+        "../../Bilder/Hintergrundbilder - schöne Landschaften/"
+    ]
+
+
+@ex.capture
+def validate(model, x, y, bs):
+    prediction = model.predict(x, batch_size=bs)
+    validation_loss = categorical_crossentropy(y, prediction)
+    return validation_loss
 
 
 @ex.automain
-def train(input_size, bs, lr, lr_decay, l1fc, l1fs, l2fc, l2fs, l3fc, l3fs):
+def train(input_size, bs, lr, lr_decay, l1fc, l1fs, l2fc, l2fs, l3fc, l3fs, image_folders):
     optimizer = Adam(lr, decay=lr_decay)
     model = create_model(input_size, l1fc, l1fs, l2fc, l2fs, l3fc, l3fs)
-    model.compile(optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
-    data_generator = UnsharpTrainingDataGenerator([
-        "../../Bilder/Backgrounds/"
-    ], batch_size=bs, target_size=input_size)
+    model.compile(optimizer, loss=categorical_crossentropy, metrics=["accuracy"])
+    data_generator = UnsharpTrainingDataGenerator(image_folders, batch_size=bs, target_size=input_size)
     for x, y in data_generator:
         model.fit(x, y, batch_size=bs, epochs=1)
+        validate(model, x, y)
