22068a9ac7cea9ee330c6e060a28e49275d43e52
Pinae <jonny@pinae.net>
2018-06-21 15:44:40 +0200

    Drawing a list with a initial size

--- inference_gui.py
+++ inference_gui.py
@@ -3,8 +3,8 @@
 from __future__ import division, print_function, unicode_literals
 from PyQt5.QtCore import Qt, QRect, pyqtSignal, QSize
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QSizePolicy
-from PyQt5.QtWidgets import QPushButton, QLabel, QFileDialog, QSplitter, QWidgetItem, QCheckBox, QScrollArea
-from PyQt5.QtGui import QImage, QPainter, QPixmap, QColor
+from PyQt5.QtWidgets import QPushButton, QLabel, QFileDialog, QSplitter, QCheckBox, QScrollArea
+from PyQt5.QtGui import QImage, QPainter, QColor
 from skimage.io import imread
 from visualization_helpers import convert_image
 import sys
@@ -13,18 +13,25 @@ import re
 
 
 class ImageWidget(QWidget):
-    def __init__(self, img_pixmap):
+    def __init__(self, img):
         super().__init__()
-        self.img = img_pixmap
+        self.img = img
+        self.rect = QRect(0, 0, 128, 128)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum))
+        self.set_img(img)
+
+    def set_img(self, img):
+        self.img = img
         if self.img:
-            print(self.img.size())
-            self.setMinimumWidth(self.img.width())
-            self.setMinimumHeight(self.img.height())
+            self.setMaximumSize(self.img.size())
+            self.setFixedSize(self.img.size())
             self.rect = QRect(0, 0, self.img.width(), self.img.height())
-            print(self.rect.size())
         else:
-            self.rect = QRect(0, 0, 20, 20)
-        print(self.size())
+            self.rect = QRect(0, 0, 128, 128)
+            self.setFixedSize(QSize(128, 128))
+        self.updateGeometry()
+        print("ImageWidget size: " + str(self.size()))
+        print("ImageWidget pos: " + str(self.pos().x()) + ", " + str(self.pos().y()))
 
     def minimumSizeHint(self):
         if self.img:
@@ -38,21 +45,18 @@ class ImageWidget(QWidget):
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
-        #qp.setRenderHint(QPainter.Antialiasing)
-        #qp.setRenderHint(QPainter.HighQualityAntialiasing)
         self.draw(qp)
         qp.end()
 
     def draw(self, qp):
-        #size = self.size()
-        #w = size.width()
-        #h = size.height()
         qp.setBrush(QColor(255, 0, 255))
-        qp.setPen(QColor(255, 128, 0))
+        qp.setPen(QColor(255, 0, 0))
         qp.drawRect(self.rect)
         if self.img:
-            qp.drawPixmap(self.rect,
-                          self.img, self.rect)
+            qp.drawImage(0, 0, self.img)
+        qp.drawLine(0, 0, 0, self.size().height())
+        qp.setPen(QColor(0, 0, 255))
+        qp.drawLine(0, 0, self.size().width(), self.size().height())
 
 
 class ThumbnailList(QWidget):
@@ -65,17 +69,20 @@ class ThumbnailList(QWidget):
         size_policy = QSizePolicy()
         size_policy.setVerticalPolicy(QSizePolicy.MinimumExpanding)
         self.setSizePolicy(size_policy)
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(4, 4, 4, 0)
         size_row = QHBoxLayout()
         slider_label = QLabel()
         slider_label.setText("Thumbnailgröße:")
         slider_label.setMinimumHeight(12)
         size_row.addWidget(slider_label, alignment=Qt.AlignLeading)
-        layout.addLayout(size_row)
+        self.layout.addLayout(size_row)
         self.t_list = QVBoxLayout()
-        layout.addLayout(self.t_list)
-        layout.addStretch()
-        self.setLayout(layout)
+        self.lines = []
+        for _ in range(8):
+            self.add_empty_line()
+        self.layout.addLayout(self.t_list, stretch=1)
+        self.setLayout(self.layout)
 
     def load_images(self, path):
         self.images = []
@@ -84,7 +91,7 @@ class ThumbnailList(QWidget):
         for filename in os.listdir(path):
             if filename_regex.match(filename):
                 np_img = imread(os.path.join(path, filename))
-                qimage = QImage(convert_image(np_img), np_img.shape[0], np_img.shape[1], QImage.Format_RGB32)
+                qimage = QImage(convert_image(np_img), np_img.shape[1], np_img.shape[0], QImage.Format_RGB32)
                 self.images.append({
                     'thumb': qimage.scaledToWidth(128),
                     'np_img': np_img,
@@ -95,21 +102,30 @@ class ThumbnailList(QWidget):
             self.imgSelected.emit(self.images[0])
 
     def update_list(self):
-        while self.t_list.count() > 0:
-            item = self.t_list.takeAt(self.t_list.count()-1)
-            if type(item) == QWidgetItem:
-                item.widget().deleteLater()
-            else:
-                item.deleteLater()
-        for img_data in self.images:
-            img_line = QHBoxLayout()
-            checkbox = QCheckBox()
-            checkbox.setText(".")
-            img_line.addWidget(checkbox, alignment=Qt.AlignLeading)
-            image = ImageWidget(QPixmap.fromImage(img_data['thumb'], flags=(Qt.AutoColor | Qt.DiffuseDither)))
-            img_line.addWidget(image, alignment=Qt.AlignLeading)
-            self.t_list.addLayout(img_line)
-        self.update()
+        while len(self.lines) < len(self.images):
+            self.add_empty_line()
+        for i, img_data in enumerate(self.images):
+            self.lines[i]['checkbox'].setVisible(True)
+            self.lines[i]['image_widget'].set_img(img_data['thumb'])
+            self.lines[i]['image_widget'].setMinimumSize(img_data['thumb'].size())
+            self.lines[i]['image_widget'].setFixedSize(img_data['thumb'].size())
+            self.lines[i]['image_widget'].setVisible(True)
+            self.lines[i]['image_widget'].updateGeometry()
+        for i in range(len(self.lines), len(self.images)):
+            self.lines[i]['checkbox'].setVisible(False)
+            self.lines[i]['image_widget'].setVisible(False)
+        self.t_list.update()
+        self.updateGeometry()
+
+    def add_empty_line(self):
+        img_line = QHBoxLayout()
+        checkbox = QCheckBox()
+        img_line.addWidget(checkbox)
+        img_wid = ImageWidget(None)
+        img_wid.setFixedSize(QSize(128, 128))
+        img_line.addWidget(img_wid)
+        self.lines.append({'checkbox': checkbox, 'image_widget': img_wid})
+        self.t_list.addLayout(img_line)
 
 
 class PreviewArea(QWidget):
@@ -120,16 +136,23 @@ class PreviewArea(QWidget):
         size_policy.setVerticalPolicy(QSizePolicy.Expanding)
         self.setSizePolicy(size_policy)
         layout = QVBoxLayout()
+        layout.setContentsMargins(0, 4, 0, 0)
+        this_row = QHBoxLayout()
+        this_row.addSpacing(4)
         selection_label = QLabel()
         selection_label.setText("Dieses Bild")
-        layout.addWidget(selection_label, alignment=Qt.AlignLeading)
+        this_row.addWidget(selection_label)
+        layout.addLayout(this_row)
+        img_scroll_area = QScrollArea()
+        img_scroll_area.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
         self.img_widget = ImageWidget(None)
-        layout.addWidget(self.img_widget, alignment=Qt.AlignLeading)
+        img_scroll_area.setWidget(self.img_widget)
+        layout.addWidget(img_scroll_area, stretch=1)
         layout.addStretch()
         self.setLayout(layout)
 
     def set_image(self, img_d):
-        self.img_widget = ImageWidget(QPixmap.fromImage(img_d['qimage'], flags=(Qt.AutoColor | Qt.DiffuseDither)))
+        self.img_widget.set_img(img_d['qimage'])
         self.update()
 
 
@@ -138,7 +161,7 @@ class InferenceInterface(QWidget):
         super().__init__(flags=Qt.WindowTitleHint | Qt.WindowCloseButtonHint |
                                Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint)
         self.path = None
-        self.setGeometry(300, 150, 800, 600)
+        self.setGeometry(200, 100, 1280, 720)
         self.setWindowTitle("Unsharp Detector")
         main_layout = QVBoxLayout()
         path_row = QHBoxLayout()
@@ -160,11 +183,12 @@ class InferenceInterface(QWidget):
         image_splitter.addWidget(scroll_area)
         self.preview_area = PreviewArea()
         image_splitter.addWidget(self.preview_area)
-        image_splitter.setSizes([158, self.width()-158])
+        image_splitter.setSizes([176, self.width()-176])
         image_splitter.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
         main_layout.addWidget(image_splitter)
         self.setLayout(main_layout)
         self.show()
+        self.thumbnail_list.load_images("./validation_data/good/")
 
     def open_path_select_dialog(self):
         dialog = QFileDialog()
