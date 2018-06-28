2d1411135f0840a0de0b7c66daa890b047503d00
Pinae <jonny@pinae.net>
2018-06-28 15:38:16 +0200

    Working delegate

---
+++ classified_image_datatype.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+from PyQt5.QtCore import QObject, QPropertyAnimation, QSequentialAnimationGroup, QEasingCurve, pyqtSignal
+from PyQt5.QtGui import QImage, QColor
+from visualization_helpers import convert_image
+
+
+class ClassifiedImageBundle(QObject):
+    UNDECIDED, CLASSIFIED, MANUAL, PROGRESS = range(4)
+    data_changed = pyqtSignal(QObject)
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.img = None
+        self.thumb = None
+        self.filename = None
+        self.np_array = None
+        self.status = ClassifiedImageBundle.UNDECIDED
+        self.color = None
+        self.keep = None
+        self.animation_progress = 1.0
+        self.ani = QSequentialAnimationGroup()
+        self.init_animation()
+
+    def set_animation_progress(self, val):
+        self.animation_progress = val
+        self.data_changed.emit(self)
+
+    def init_animation(self):
+        ani1 = QPropertyAnimation(self, b"animation_progress")
+        ani1.setDuration(1700)
+        ani1.setEasingCurve(QEasingCurve.InOutQuad)
+        ani1.setStartValue(-0.001)
+        ani1.setEndValue(-1.0)
+        ani1.valueChanged.connect(self.set_animation_progress)
+        self.ani.addAnimation(ani1)
+        ani2 = QPropertyAnimation(self, b"animation_progress")
+        ani2.setDuration(2300)
+        ani2.setEasingCurve(QEasingCurve.InOutQuad)
+        ani2.setStartValue(0.0)
+        ani2.setEndValue(1.0)
+        ani2.valueChanged.connect(self.set_animation_progress)
+        self.ani.addAnimation(ani2)
+        self.ani.setLoopCount(-1)
+
+    def set_np_image(self, np_array):
+        self.np_array = np_array
+        self.img = QImage(convert_image(np_array), np_array.shape[1], np_array.shape[0], QImage.Format_RGB32)
+        self.thumb = self.img.scaledToWidth(128)
+
+    def set_filename(self, filename):
+        self.filename = filename
+
+    def set_image_from_filename(self, filename):
+        self.filename = filename
+        self.img = QImage(filename)
+        self.thumb = self.img.scaledToWidth(128)
+
+    def set_progress(self):
+        self.status = ClassifiedImageBundle.PROGRESS
+        self.color = QColor(148, 148, 255)
+        self.ani.start()
+
+    def set_manual(self, decision):
+        self.keep = decision
+        self.status = ClassifiedImageBundle.MANUAL
+        if self.keep:
+            self.color = QColor(0, 255, 0)
+        else:
+            self.color = QColor(255, 0, 0)
+        self.ani.stop()
+        self.animation_progress = 1.0
+
+    def set_classification(self, result):
+        if self.status != ClassifiedImageBundle.MANUAL:
+            self.keep = result[0] > result[1]
+            self.status = ClassifiedImageBundle.CLASSIFIED
+            self.color = QColor(int(result[1] * 255), int(result[0] * 255), 0)
+            self.ani.stop()
+            self.animation_progress = 1.0
+
+    def get_thumb(self):
+        return self.thumb
+
+    def get_image(self):
+        return self.img
+
+    def has_color(self):
+        return self.color is not None and \
+               self.status in [ClassifiedImageBundle.CLASSIFIED,
+                               ClassifiedImageBundle.MANUAL,
+                               ClassifiedImageBundle.PROGRESS]
+
+    def get_color(self):
+        return self.color
+
+    def get_animation_progress(self):
+        return self.animation_progress
---
+++ extended_qt_delegate.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+from PyQt5.QtCore import Qt, QSize, QLineF
+from PyQt5.QtWidgets import QStyledItemDelegate
+from PyQt5.QtGui import QPen, QBrush, QPainter
+from classified_image_datatype import ClassifiedImageBundle
+
+
+class ImageableStyledItemDelegate(QStyledItemDelegate):
+    def __init__(self, parent=None, *args):
+        super().__init__(*args)
+        self.setParent(parent)
+
+    def paint(self, qp, style_option_view_item, model_index):
+        mid = model_index.data()
+        if type(mid) is ClassifiedImageBundle:
+            qp.save()
+            qp.drawImage(style_option_view_item.rect.left() + 4, style_option_view_item.rect.top() + 4, mid.get_thumb())
+            qp.setRenderHint(QPainter.Antialiasing)
+            qp.setRenderHint(QPainter.HighQualityAntialiasing)
+            if mid.has_color():
+                qp.setPen(QPen(QBrush(mid.get_color()), 4.0, Qt.SolidLine, Qt.SquareCap, Qt.RoundJoin))
+                lines_to_draw = []
+                len_of_all_lines = 2 * (mid.get_thumb().height() + mid.get_thumb().width() + 12)
+                line_start_pos = -1 * min(0, mid.get_animation_progress()) * len_of_all_lines
+                if mid.get_animation_progress() >= 0:
+                    line_end_pos = mid.get_animation_progress() * len_of_all_lines
+                else:
+                    line_end_pos = 1 * len_of_all_lines
+                tx = style_option_view_item.rect.left() + 2
+                ty = style_option_view_item.rect.top() + 2
+                h = mid.get_thumb().height() + 4
+                w = mid.get_thumb().width() + 4
+                if line_start_pos <= h and 0 < line_end_pos:
+                    lines_to_draw.append(QLineF(tx,
+                                                ty + line_start_pos,
+                                                tx,
+                                                ty + min(h, line_end_pos)))
+                if line_start_pos <= h + w and h < line_end_pos:
+                    lines_to_draw.append(QLineF(tx + max(line_start_pos, h) - h,
+                                                ty + h,
+                                                tx + min(h + w, line_end_pos) - h,
+                                                ty + h))
+                if line_start_pos <= 2 * h + w and h + w < line_end_pos:
+                    lines_to_draw.append(QLineF(tx + w,
+                                                ty + h - (max(line_start_pos - h - w, 0)),
+                                                tx + w,
+                                                ty + h - (min(line_end_pos - h - w, h))))
+                if line_start_pos <= 2 * h + 2 * w and 2 * h + w < line_end_pos:
+                    lines_to_draw.append(QLineF(tx + w - (max(line_start_pos - 2 * h - w, 0)),
+                                                ty,
+                                                tx + w - (min(line_end_pos - 2 * h - w, w)),
+                                                ty))
+                qp.drawLines(lines_to_draw)
+            qp.restore()
+        else:
+            super().paint(qp, style_option_view_item, model_index)
+
+    def sizeHint(self, style_option_view_item, model_index):
+        mid = model_index.data()
+        if type(mid) is ClassifiedImageBundle:
+            return QSize(mid.get_thumb().width() + 8, mid.get_thumb().height() + 8)
+        else:
+            return super().sizeHint(style_option_view_item, model_index)
+
+    def editorEvent(self, event, model, style_option_view_item, model_index):
+        print("called editorEvent")
+        print(event)
+        print(model)
+        return super().editorEvent(event, model, style_option_view_item, model_index)
+
+    def createEditor(self, parent, style_option_view_item, model_index):
+        print("called createEditor")
+        print(parent)
+        return super().createEditor(parent, style_option_view_item, model_index)
+
+    def commitData(self, editor):
+        print("called commitData")
+        print(editor)
+        return super().commitData(editor)
+
+    def setEditorData(self, editor, model_index):
+        print("called setEditorData")
+        print(editor)
+        return super().setEditorData(editor, model_index)
