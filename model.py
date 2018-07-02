cf1df9850a7eebba72bf84d274bbb69a8e37c59c
Pinae <jonny@pinae.net>
2018-07-02 18:02:34 +0200

    More features in the last layer

--- model.py
+++ model.py
@@ -26,16 +26,18 @@ def create_model(input_shape, l1fc, l1fs, l2fc, l2fs, l3fc, l3fs, eac_size):
     c1 = Conv2D(l1fc, kernel_size=l1fs, strides=2, use_bias=True, padding="same",
                 data_format="channels_last", kernel_initializer=laplacian_group_initializer)(inp)
     l1 = LeakyReLU(alpha=0.2)(c1)
+    eac1 = EdgeAndCenterExtractionLayer(width=eac_size)(l1)
     c2 = Conv2D(l2fc, kernel_size=l2fs, strides=2, use_bias=True, padding="same",
                 data_format="channels_last")(l1)
     l2 = LeakyReLU(alpha=0.2)(c2)
+    eac2 = EdgeAndCenterExtractionLayer(width=eac_size)(l2)
     c3 = Conv2D(l3fc, kernel_size=l3fs, strides=1, use_bias=True, padding="same",
                 data_format="channels_last")(l2)
-    eac = EdgeAndCenterExtractionLayer(width=eac_size)(c3)
-    eac_max_grid = MaxPool2D((eac_size, eac_size), strides=eac_size,
-                             padding="valid", data_format="channels_last")(eac)
-    eac_avg_grid = AveragePooling2D((eac_size, eac_size), strides=eac_size,
-                                    padding="valid", data_format="channels_last")(eac)
+    eac3 = EdgeAndCenterExtractionLayer(width=eac_size)(c3)
+    eac3_max_grid = MaxPool2D((eac_size, eac_size), strides=eac_size,
+                              padding="valid", data_format="channels_last")(eac3)
+    eac3_avg_grid = AveragePooling2D((eac_size, eac_size), strides=eac_size,
+                                     padding="valid", data_format="channels_last")(eac3)
     features = [GlobalVarianceLayer()(c1),
                 GlobalVarianceLayer()(c2),
                 GlobalVarianceLayer()(c3),
@@ -45,14 +47,22 @@ def create_model(input_shape, l1fc, l1fs, l2fc, l2fs, l3fc, l3fs, eac_size):
                 GlobalAveragePooling2D(data_format="channels_last")(c1),
                 GlobalAveragePooling2D(data_format="channels_last")(c2),
                 GlobalAveragePooling2D(data_format="channels_last")(c3),
-                GlobalMaxPool2D(data_format="channels_last")(eac),
-                GlobalAveragePooling2D(data_format="channels_last")(eac),
-                GlobalVarianceLayer()(eac),
-                Flatten()(VarianceLayer((eac_size, eac_size))(eac))
-                #Flatten()(VarianceLayer((eac_size, eac_size))(eac_max_grid)),
-                #Flatten()(VarianceLayer((eac_size, eac_size))(eac_avg_grid)),
-                #Flatten()(eac_max_grid),
-                #Flatten()(eac_avg_grid)
+                GlobalMaxPool2D(data_format="channels_last")(eac1),
+                GlobalMaxPool2D(data_format="channels_last")(eac2),
+                GlobalMaxPool2D(data_format="channels_last")(eac3),
+                GlobalAveragePooling2D(data_format="channels_last")(eac1),
+                GlobalAveragePooling2D(data_format="channels_last")(eac2),
+                GlobalAveragePooling2D(data_format="channels_last")(eac3),
+                GlobalVarianceLayer()(eac1),
+                GlobalVarianceLayer()(eac2),
+                GlobalVarianceLayer()(eac3),
+                Flatten()(VarianceLayer((eac_size, eac_size))(eac1)),
+                Flatten()(VarianceLayer((eac_size, eac_size))(eac2)),
+                Flatten()(VarianceLayer((eac_size, eac_size))(eac3)),
+                GlobalVarianceLayer()(eac3_max_grid),
+                GlobalVarianceLayer()(eac3_avg_grid),
+                Flatten()(eac3_max_grid),
+                Flatten()(eac3_avg_grid)
                 ]
     feature_vector = Concatenate()(features)
     o = Dense(2, activation="softmax", use_bias=True, name="output")(feature_vector)
